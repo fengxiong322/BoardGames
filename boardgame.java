8a48ed83b645ea7affebb52bec20c53296936421
fengxiong322 <xiongfeng416@gmail.com>
2019-02-14 19:22:28 -0500

    Update boardgame.java

--- boardgame.java
+++ boardgame.java
@@ -1,14 +1,19 @@
 
 public class BoardGames(){
   //Variables
-  private int numOfPlaceMarkers;
-  private String shapeOfBoard;
-  private int lengthOfBoard;
-  public BoardGames(){
+  private String name;
+  private int numOfPieces;//Number of pieces
+  private String pieces[][];//For each piece, x and y coords.
+  private int heightOfBoard;//Number of positions on the y axis
+  private int widthOfBoard;//Number of positions on the x axis
+  private int[][] board;//The full board
+  public BoardGames(String name, int heightOfBoard, int widthOfBoard,){
+    this.name = name;
+    this.heightOfBoard = heightOfBoard;
+    this.widthOfBoard = widthOfBoard;
+    board = new int[widthOfBoard][heightOfBoard];
     
   }
   
-  public void setBhaldasdaw
+  public void setPiece
 }
-
-  No u :)
