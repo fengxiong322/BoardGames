9c2f048d25bd2c7b99a1d18237802400b7dbe7c6
fengxiong322 <xiongfeng416@gmail.com>
2019-02-14 20:09:42 -0500

    Update boardgame.java
    Creating new get/set methods

--- boardgame.java
+++ boardgame.java
@@ -6,14 +6,36 @@ public class BoardGames(){
   private String pieces[][];//For each piece, x and y coords.
   private int heightOfBoard;//Number of positions on the y axis
   private int widthOfBoard;//Number of positions on the x axis
-  private int[][] board;//The full board
-  public BoardGames(String name, int heightOfBoard, int widthOfBoard,){
+  private int[][] board;//The full board. -1 means empty. any other number means the piece number
+  private int turns;
+
+  public BoardGames(String name, int heightOfBoard, int widthOfBoard, int numOfPieces){
     this.name = name;
     this.heightOfBoard = heightOfBoard;
     this.widthOfBoard = widthOfBoard;
     board = new int[widthOfBoard][heightOfBoard];
-    
+    this.numOfPieces = numOfPieces;
+    pieces = new int[numOfPieces][2];
   }
   
-  public void setPiece
+  public boolean setPiece(int pieceNumber, int x, int y){
+  	if(board[x][y]==-1 && x>=0 && x<widthOfBoard && y>=0 && y<heightOfBoard){
+  		pieces[pieceNumber][0] = x;
+  		pieces[pieceNumber][1] = y;
+  		return true;
+  	}
+  	return false;
+  }
+
+  public String getName(){
+  	return name;
+  }
+
+  public int getNumOfPieces(){
+  	return numOfPieces;
+  }
+
+  public void iterateTurns(){
+  	turns++;
+  }
 }
