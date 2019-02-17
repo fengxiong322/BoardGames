6f152567cf388f2bebd3380f714edb16a68fd67e
rhapsody7893 <47641961+rhapsody7893@users.noreply.github.com>
2019-02-17 16:56:29 -0500

    Update SnakesAndLadders.java

--- SnakesAndLadders.java
+++ SnakesAndLadders.java
@@ -112,18 +112,3 @@ public class SnakesAndLadders extends BoardGames{
         return i;
     return -1;
   }
-
-  public static void main(String[] args) {
-    SnakesAndLadders sal = new SnakesAndLadders(2, 1, 1);
-    System.out.println("\t\t\tYou are playing " + sal.getName() + "!");
-    int dice;
-    do{
-      dice = rollDice();
-      System.out.println("Turn number "+ sal.getTurns());
-      sal.displayBoard();
-      sal.iterateTurns();
-      System.out.println("Player " + sal.getCurrentPlayer() + " has rolled a " + dice + "!");
-    }while(!sal.move(dice));//While no one has won yet
-    System.out.println("Player number " + sal.getCurrentPlayer() + " has won!");
-  }
-}
