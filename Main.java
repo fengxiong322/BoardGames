71d05121b92da76f6c49d4ca89a2017a096f9e3b
rhapsody7893 <47641961+rhapsody7893@users.noreply.github.com>
2019-02-18 14:33:35 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,26 @@
   
-  public static void main(String[] args) {
-    SnakesAndLadders sal = new SnakesAndLadders(2, 1, 1);
-    System.out.println("\t\t\tYou are playing " + sal.getName() + "!");
-    int dice;
-    do{
-      dice = rollDice();
-      System.out.println("Turn number "+ sal.getTurns());
-      sal.displayBoard();
-      sal.iterateTurns();
-      System.out.println("Player " + sal.getCurrentPlayer() + " has rolled a " + dice + "!");
-    }while(!sal.move(dice));//While no one has won yet
-    System.out.println("Player number " + sal.getCurrentPlayer() + " has won!");
-  }
+ package lesson2;
+
+/**
+ * @author xiaoming
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SnakesAndLadders sal = new SnakesAndLadders(2, 1, 1);
+		System.out.println("\t\t\tYou are playing " + sal.getName() + "!");
+		int dice;
+		do {
+			dice = SnakesAndLadders.rollDice();
+			System.out.println("Turn number " + sal.getTurns());
+			sal.displayBoard();
+			sal.iterateTurns();
+			System.out.println("Player " + sal.getCurrentPlayer() + " has rolled a " + dice + "!");
+		} while (!sal.move(dice));// While no one has won yet
+		System.out.println("Player number " + sal.getCurrentPlayer() + " has won!");
+	}
 }
