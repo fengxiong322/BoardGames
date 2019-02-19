9b2407956c9242f093501851fdac09f5504024ea
rhapsody7893 <47641961+rhapsody7893@users.noreply.github.com>
2019-02-18 21:56:38 -0500

    Update SnakesAndLadders.java

--- SnakesAndLadders.java
+++ SnakesAndLadders.java
@@ -1,167 +1,196 @@
 
+package lesson2;
+
 /**
+ * This class represents a snakes and ladders game
  * @author Feng, Michael
- *
- */ 
+ */
 public class SnakesAndLadders extends BoardGames {
-  private int[][] snakes;
-  private int[][] ladders;
-  private int numSnakes;
-  private int numLadders;
-  private int currentPlayer;
+	private int[][] snakes;
+	private int[][] ladders;
+	private int numSnakes;
+	private int numLadders;
 
-  /** Constructor - sets values for number of players, number of snakes, and number of ladders
-   * @param players - number of players
-   * @param numOfSnakes - number of snakes
-   * @param numOfLadders - number of ladders
-   */
-  public SnakesAndLadders(int players, int numOfSnakes, int numOfLadders) {
-    super("Snakes and Ladders", 10, 10, players);// Creates a ten by ten board
-    numSnakes = numOfSnakes;
-    numLadders = numOfLadders;
-    snakes = new int[numSnakes][4];// 0 is start x coord, 1 is start y coord, 2 is end x coord, 3 is end y coord
-    for (int i = 0; i < numSnakes; i++) {
-      for (int j = 0; j < 4; j++)
-        snakes[i][j] = 0;
-    }
-    ladders = new int[numLadders][4];// 0 is start x coord, 1 is start y coord, 2 is end x coord, 3 is end y coord
-    for (int i = 0; i < numLadders; i++) {
-      for (int j = 0; j < 4; j++)
-        ladders[i][j] = 0;
-    }
-    currentPlayer = 0;
-  }
+	public int getNumSnakes() {
+		return numSnakes;
+	}
 
-  /** Returns a random value between 1 to 6
-   * @return a  a random value between 1 to 6
-   */
-  public static int rollDice() {
-    return (int) (Math.random() * 6 + 1);
-  }
+	public int getNumLadders() {
+		return numLadders;
+	}
 
-  /** Creates a certain snake starting at a certain x and y position and ending at a certain x and y position
-   * @param snakeNum - which snake
-   * @param startX - start x cord
-   * @param startY - start y cord
-   * @param endX - end x cord
-   * @param endY - end y cord
-   * @return if the snake was generated successfully
-   */
-  public boolean generateSnake(int snakeNum, int startX, int startY, int endX, int endY) {// snakeNum is zero indexed
-    if (snakeNum < numSnakes && snakeNum >= 0)
-      if (startX < 10 && startX >= 0)
-        if (startY < 10 && startY >= 0)
-          if (endX < 10 && endX >= 0)
-            if (endY < 10 && endY >= 0) {
-              snakes[snakeNum][0] = startX;
-              snakes[snakeNum][1] = startY;
-              snakes[snakeNum][2] = endX;
-              snakes[snakeNum][3] = endY;
-              return true;
-            }
-    return false;
-  }
+	/**
+	 * Constructor - sets values for number of players, number of snakes, and number
+	 * of ladders and remaining default values 
+	 * @param players - number of players
+	 * @param numOfSnakes - number of snakes
+	 * @param numOfLadders - number of ladders
+	 */
+	public SnakesAndLadders(int players, int numOfSnakes, int numOfLadders) {
+		super("Snakes and Ladders", 10, 10, players);// Creates a ten by ten board
+		numSnakes = numOfSnakes;
+		numLadders = numOfLadders;
+		snakes = new int[numSnakes][4];// 0 is start x coord, 1 is start y coord, 2 is end x coord, 3 is end y coord
+		for (int i = 0; i < numSnakes; i++) {
+			for (int j = 0; j < 4; j++)
+				snakes[i][j] = 0;
+		}
+		ladders = new int[numLadders][4];// 0 is start x coord, 1 is start y coord, 2 is end x coord, 3 is end y coord
+		for (int i = 0; i < numLadders; i++) {
+			for (int j = 0; j < 4; j++)
+				ladders[i][j] = 0;
+		}
+	}
 
-  /** Creates a certain ladder starting at a certain x and y position and ending at a certain x and y position
-   * @param ladderNum - which ladder
-   * @param startX - start x cord
-   * @param startY - start y cord
-   * @param endX - end x cord
-   * @param endY - end y cord
-   * @return if the ladder was generated successfully
-   */
-  public boolean generateLadder(int ladderNum, int startX, int startY, int endX, int endY) {// ladderNum is zero
-                                                // indexed
-    if (ladderNum < numLadders && ladderNum >= 0)
-      if (startX < 10 && startX >= 0)
-        if (startY < 10 && startY >= 0)
-          if (endX < 10 && endX >= 0)
-            if (endY < 10 && endY >= 0) {
-              ladders[ladderNum][0] = startX;
-              ladders[ladderNum][1] = startY;
-              ladders[ladderNum][2] = endX;
-              ladders[ladderNum][3] = endY;
-              return true;
-            }
-    return false;
-  }
+	/**
+	 * Constructor - sets value for number of players and remaining default values
+	 * @param players - number of players
+	 */
+	public SnakesAndLadders(int players) {
+		super("Snakes and Ladders", 10, 10, players);// Creates a ten by ten board
+		numSnakes = 0;
+		numLadders = 0;
+		snakes = new int[numSnakes][4];// 0 is start x coord, 1 is start y coord, 2 is end x coord, 3 is end y coord
+		for (int i = 0; i < numSnakes; i++) {
+			for (int j = 0; j < 4; j++)
+				snakes[i][j] = 0;
+		}
+		ladders = new int[numLadders][4];// 0 is start x coord, 1 is start y coord, 2 is end x coord, 3 is end y coord
+		for (int i = 0; i < numLadders; i++) {
+			for (int j = 0; j < 4; j++)
+				ladders[i][j] = 0;
+		}
+	}
 
-  /** Returns the value of the current player
-   * @return the current player
-   */
-  public int getCurrentPlayer() {
-    return currentPlayer;
-  }
+	/**
+	 * Creates a certain snake starting at a certain x and y position and ending at
+	 * a certain x and y position
+	 * @param snakeNum - which snake
+	 * @param startX - start x cord
+	 * @param startY - start y cord
+	 * @param endX - end x cord
+	 * @param endY - end y cord
+	 * @return if the snake was generated successfully
+	 */
+	public boolean generateSnake(int snakeNum, int startX, int startY, int endX, int endY) {// snakeNum is zero indexed
+		if (snakeNum < numSnakes && snakeNum >= 0)
+			if (startX < 10 && startX >= 0)
+				if (startY < 10 && startY >= 0)
+					if (endX < 10 && endX >= 0)
+						if (endY < 10 && endY >= 0) {
+							snakes[snakeNum][0] = startX;
+							snakes[snakeNum][1] = startY;
+							snakes[snakeNum][2] = endX;
+							snakes[snakeNum][3] = endY;
+							return true;
+						}
+		return false;
+	}
 
-  /** 
-   *A helper method to move the pieces around the board 
-   */
-  private void moveOnce() {
-    int x = getPieceX(currentPlayer);
-    int y = getPieceY(currentPlayer);
-    if (y % 2 == 0) {//If y is even
-      if (x == 9){
-        y++;
-        setPiece(currentPlayer, x, y);// Move up the y
-      }else{
-        x++;
-        setPiece(currentPlayer, x, y);// Move up the x
-      }
-    } else {
-      if (x == 0 && y != 0){
-        y++;
-        setPiece(currentPlayer, x, y);// Move up the y
-      } else {
-        x--;
-        setPiece(currentPlayer, x, y);
-      }
-    }
-  }
+	/**
+	 * Creates a certain ladder starting at a certain x and y position and ending at
+	 * a certain x and y position
+	 * @param ladderNum - which ladder
+	 * @param startX - start x cord
+	 * @param startY - start y cord
+	 * @param endX - end x cord
+	 * @param endY - end y cord
+	 * @return if the ladder was generated successfully
+	 */
+	public boolean generateLadder(int ladderNum, int startX, int startY, int endX, int endY) {// ladderNum is zero
+		// indexed
+		if (ladderNum < numLadders && ladderNum >= 0)
+			if (startX < 10 && startX >= 0)
+				if (startY < 10 && startY >= 0)
+					if (endX < 10 && endX >= 0)
+						if (endY < 10 && endY >= 0) {
+							ladders[ladderNum][0] = startX;
+							ladders[ladderNum][1] = startY;
+							ladders[ladderNum][2] = endX;
+							ladders[ladderNum][3] = endY;
+							return true;
+						}
+		return false;
+	}
 
-  /** moves the current player, checks if the player won
-   * @param numOfMoves
-   * @return
-   */
-  public boolean move(int numOfMoves) {// returns true if the moving player has won
-    setBoard(getPieceX(currentPlayer),getPieceY(currentPlayer), -1);
-    for (int i = 0; i < numOfMoves; i++) {
-      moveOnce();
-      if (getPieceY(currentPlayer) == 9 && getPieceX(currentPlayer) == 0)// when at the top row and first column
-        return true;// player has won
-    }
-    int currentSnake = checkSnake();
-    if (currentSnake != -1) {
-      setPiece(currentPlayer, snakes[currentSnake][2], snakes[currentSnake][3]);
-    }
-    int currentLadder = checkLadder();
-    if (currentLadder != -1) {
-      setPiece(currentPlayer, ladders[currentSnake][2], ladders[currentSnake][3]);
-    }
-    setBoard(getPieceX(currentPlayer),getPieceY(currentPlayer), currentPlayer);
-    if (currentPlayer == getNumOfPieces() - 1)
-      currentPlayer = 0;
-    else
-      currentPlayer++;
-    return false;
-  }
+	/**
+	 * A helper method to move the pieces around the board
+	 */
+	private void moveOnce() {
+		int x = getPieceX(getCurrentPlayer());
+		int y = getPieceY(getCurrentPlayer());
+		if (y % 2 == 0) {// If y is even
+			if (x == 9) {
+				y++;
+				setPiece(getCurrentPlayer(), x, y);// Move up the y
+			} else {
+				x++;
+				setPiece(getCurrentPlayer(), x, y);// Move up the x
+			}
+		} else {
+			if (x == 0 && y != 0) {
+				y++;
+				setPiece(getCurrentPlayer(), x, y);// Move up the y
+			} else {
+				x--;
+				setPiece(getCurrentPlayer(), x, y);// Move up the x
+			}
+		}
+	}
 
-  /** Returns the snake number, or -1 if there is no such snake
-   * @return the snake number, or -1 if there is no such snake
-   */
-  public int checkSnake() {// returns the snake number, or -1 if there is no such snake
-    for (int i = 0; i < numSnakes; i++)
-      if (snakes[i][0] == getPieceX(currentPlayer) && snakes[i][1] == getPieceY(currentPlayer))
-        return i;
-    return -1;
-  }
+	/**
+	 * moves the current player, checks if the player won 
+	 * @param numOfMoves
+	 * @return of the moving player has won
+	 */
+	public boolean moveAndCheck (int numOfMoves) {// returns true if the moving player has won
+		setBoard(getPieceX(getCurrentPlayer()), getPieceY(getCurrentPlayer()), -1);
+		for (int i = 0; i < numOfMoves; i++) {
+			moveOnce();
+			if (getPieceY(getCurrentPlayer()) == 9 && getPieceX(getCurrentPlayer()) == 0)// when at the top row and
+																							// first column
+				return true;// player has won
+		}
+		int currentSnake = checkSnake();
+		if (currentSnake != -1) {
+			System.out.println("You landed on a snake, you slid down to " + snakes[currentSnake][0] + ","
+					+ snakes[currentSnake][1]);
+			setPiece(getCurrentPlayer(), snakes[currentSnake][0], snakes[currentSnake][1]);
+		}
+		int currentLadder = checkLadder();
+		if (currentLadder != -1) {
+			System.out.println("You landed on a ladder, you climbed up to " + ladders[currentLadder][2] + ","
+					+ ladders[currentLadder][3]);
+			setPiece(getCurrentPlayer(), ladders[currentLadder][2], ladders[currentLadder][3]);
+		}
+		setBoard(getPieceX(getCurrentPlayer()), getPieceY(getCurrentPlayer()), getCurrentPlayer());
+		if (getCurrentPlayer() == getNumOfPieces() - 1)
+			setCurrentPlayer(0);
+		else
+			nextPlayer();
+		return false;
+	}
 
-  /** Returns the ladder number, or -1 if there is no such ladder
-   * @return the ladder number, or -1 if there is no such laddeR
-   */
-  public int checkLadder() {// returns the ladder number, or -1 if there is no such ladder
-    for (int i = 0; i < numLadders; i++)
-      if (ladders[i][0] == getPieceX(currentPlayer) && ladders[i][1] == getPieceY(currentPlayer))
-        return i;
-    return -1;
-  }
+	/**
+	 * Returns the snake number, or -1 if there is no such snake
+	 * @return the snake number, or -1 if there is no such snake
+	 */
+	public int checkSnake() {// returns the snake number, or -1 if there is no such snake
+		for (int i = 0; i < numSnakes; i++)
+			if (snakes[i][2] == getPieceX(getCurrentPlayer()) && snakes[i][3] == getPieceY(getCurrentPlayer()))
+				return i;
+		return -1;
+	}
+
+	/**
+	 * Returns the ladder number, or -1 if there is no such ladder
+	 * @return the ladder number, or -1 if there is no such ladder
+	 */
+	public int checkLadder() {// returns the ladder number, or -1 if there is no such ladder
+		for (int i = 0; i < numLadders; i++)
+			if (ladders[i][0] == getPieceX(getCurrentPlayer()) && ladders[i][1] == getPieceY(getCurrentPlayer()))
+				return i;
+		return -1;
+	}
+
 }
